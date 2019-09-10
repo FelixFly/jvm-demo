1e2a82f7259a494ca106f9f3b03991e5305dd2bc
FelixFly <chenglinxu@yeah.net>
2019-09-10 16:47:04 +0800

    init project

---
+++ src/main/java/top/felixfly/jvm/App.java
@@ -0,0 +1,13 @@
+package top.felixfly.jvm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
